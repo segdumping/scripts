9a04b6a809c3cb0d431c89d0bdedef1cc887cd61
龙吟泉 <longyinquan@longyinandeMini.bbrouter>
2019-08-25 09:28:17 +0800

    curl静默模式

--- bingwallpaper.sh
+++ bingwallpaper.sh
@@ -3,12 +3,13 @@
 #必应壁纸api
 url="https://www.bing.com/HPImageArchive.aspx?format=js&n=8"
 rootpath="/Users/$USER/Pictures/"
+errpath="${rootpath}error.log"
 #结果存入文件
 jsonpath=${rootpath}wallpaper.json
 #数据信息不存在直接下载
-curl="curl --retry 3"
+curl="curl -s -S --retry 3"
 if [[ ! -s $jsonpath ]];then
-        $curl $url > $jsonpath 2>>${rootpath}err.log
+        $curl -o $jsonpath $url 2>>$errpath
 fi
 #最近的壁纸日期
 lastestdate=$(cat $jsonpath | /usr/local/bin/jq -r ".images[0].enddate")
@@ -16,11 +17,11 @@ lastestdate=$(cat $jsonpath | /usr/local/bin/jq -r ".images[0].enddate")
 today=$(date +%Y%m%d)
 #下载最新的信息
 if [[ $lastestdate -lt $today ]];then
-        $curl $url > $jsonpath 2>>${rootpath}err.log
+        $curl -o $jsonpath $url 2>>$errpath
 fi
 #随机数
 random=$(expr $RANDOM % 8)
-echo $random >> ${rootpath}debug.log
+echo "random="$random >> ${rootpath}debug.log
 #壁纸url
 imgurl=$(cat $jsonpath | /usr/local/bin/jq -r ".images[$random].url")
 #添加域名
@@ -30,7 +31,7 @@ imgname=$(expr "$imgurl" : '.*ZH-CN\(.*\)&rf')
 #壁纸路径
 imgpath=${rootpath}$imgname
 #不存在则先下载
-if [[ ! -e $imgpath ]]; then
+if [[ ! -s $imgpath ]]; then
         $curl -o $imgpath $imgurl
 fi
 #调用Finder应用切换桌面壁纸
